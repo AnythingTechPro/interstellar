eace25e9c3d8e9abc4fdbdb1ac3caf4a61306cc0
AnythingTechPro <anythingtechpro@gmail.com>
2017-09-29 02:34:20 +0000

    interstellar/main: code cleanup

--- interstellar/main.py
+++ interstellar/main.py
@@ -5,12 +5,12 @@ from interstellar import game, scene
 def main():
     pygame.display.init()
     pygame.mixer.init(44100, size=-16, channels=2, buffer=1024)
-    pygame.mixer.set_num_channels(32)
+    pygame.mixer.set_num_channels(16)
     pygame.joystick.init()
 
     __builtin__.game = game.Game(scene.MainMenu)
-    __builtin__.game.setup()
-    __builtin__.game.mainloop()
+    game.setup()
+    game.mainloop()
 
 if __name__ == '__main__':
     main()
